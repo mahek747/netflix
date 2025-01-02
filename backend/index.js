d432ced47f06d926daf7f6bf543a08b9f80d5159
mahekgajera1 <135087896+mahek747@users.noreply.github.com>
2025-01-02 22:32:10 +0530

    commitedd!!!

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,38 @@
-import express from 'express';
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+// import movieRoutes from "./routes/movie.route.js";
+// import tvRoutes from "./routes/tv.route.js";
+// import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/database.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
-app.get('/signup', (req,res) =>{
-    res.send("signup route"); 
-});
-app.get('/login', (req,res) =>{
-    res.send("login route"); 
-});
-app.get('/logout', (req,res) =>{
-    res.send("logout route"); 
-});
-app.listen(8000, () => {
-    console.log(`Server is running on port 8000!!`);
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/movie", protectRoute, movieRoutes);
+// app.use("/api/v1/tv", protectRoute, tvRoutes);
+// app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log("Server is running on port" + PORT);
+	connectDB();
 });
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,13 @@
-import express from 'express';
+import express from "express";
+import { authCheck, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+
 const router = express.Router();
 
-router.get("/signup", (req,res) =>{
-    res.send("signup route!!");
-});
-router.get("/login", (req,res) =>{
-    res.send("login route!!");
-});
-router.get("/logout", (req,res) =>{
-    res.send("logout route!!");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
+router.get("/authCheck", protectRoute, authCheck);
 
 export default router;
