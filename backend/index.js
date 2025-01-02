34a5d2647eeb7a614ffa47a1bb480ee1ba149b2b
mahekgajera1 <135087896+mahek747@users.noreply.github.com>
2025-01-02 22:50:38 +0530

    commiteddd!!

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-// import movieRoutes from "./routes/movie.route.js";
-// import tvRoutes from "./routes/tv.route.js";
-// import searchRoutes from "./routes/search.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/database.js";
@@ -16,13 +16,13 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json()); // will allow us to parse req.body
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-// app.use("/api/v1/movie", protectRoute, movieRoutes);
-// app.use("/api/v1/tv", protectRoute, tvRoutes);
-// app.use("/api/v1/search", protectRoute, searchRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 if (ENV_VARS.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
